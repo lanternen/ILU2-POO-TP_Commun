0a24f40e16237812631f9ef0cb0eff2c71be6965
lanternen <leo.audouin@univ-tlse3.fr>
2024-04-10 11:53:37 +0200

    TP5 POO (commun). Pas grand chose de nouveau. Les tests pour calendrier annuel passent pas

--- src/model/CalendrierAnnuel.java
+++ src/model/CalendrierAnnuel.java
@@ -1,9 +1,74 @@
 package model;
 
 public class CalendrierAnnuel {
-
+	private Mois[] calendrier;
+	
 	public CalendrierAnnuel() {
-		// TODO Auto-generated constructor stub
+		
+		calendrier[0] = new Mois("Janvier", 31);
+		calendrier[1] = new Mois("Février", 28);
+		calendrier[2] = new Mois("Mars", 31);
+		calendrier[3] = new Mois("Avril", 30);
+		calendrier[4] = new Mois("Mai", 31);
+		calendrier[5] = new Mois("Juin", 30);
+		calendrier[6] = new Mois("Juillet", 31);
+		calendrier[7] = new Mois("Aout", 31);
+		calendrier[8] = new Mois("Septembre", 30);
+		calendrier[9] = new Mois("Octobre", 31);
+		calendrier[10] = new Mois("Novembre", 30);
+		calendrier[11] = new Mois("Décembre", 31);
 	}
 
+	
+	public boolean estLibre(int jour, int mois) {
+		return calendrier[mois].estLibre(jour);
+	}
+	
+	public boolean reserver(int jour, int mois) {
+		try {
+			calendrier[mois].reserver(jour);
+			return true;
+		} catch (Throwable throwable) {
+			return false;
+		}
+	}
+	
+	
+	////////////////////////////////////////////////////////////////////////////////
+	//////// --------------------- classe interne mois --------------------////////
+	//////////////////////////////////////////////////////////////////////////////
+	
+	public class Mois {
+		
+		String nom;
+		boolean jours[];
+		
+		private Mois (String nom, int nbJours) {
+			this.nom = nom;
+			this.jours = new boolean[nbJours];	// énorme doute ici
+												//non c'est bon, mais pas besoin de faire une boucle pour mettre false
+												//maintenant, en java, c'est mis par défaut à false
+		}
+		
+		private boolean estLibre(int jour) {
+			return !(jours[jour]);		//court, élégant
+		}
+		
+		private void reserver(int jour) {
+			
+			if (estLibre(jour))		//si true, ça veut dire qu'il y a déjà une réservation
+			{
+				throw new IllegalStateException ("déjà réservé");
+			} else {
+				jours[jour] = true;
+			}
+	
+		}
+			
+	}
+	
+	
+	
+	
+	
 }
--- test/model/CalendrierAnnuelTest.java
+++ test/model/CalendrierAnnuelTest.java
@@ -26,7 +26,7 @@ class CalendrierAnnuelTest {
 	@Test
 	void testReserver() {
 		assertTrue(calendrier.reserver(20,10), "reservation du 20/10 devrait être possible");
-		assertFalse(calendrier.estLibre(20,10), "20/10 devrait être occuper");
+		assertFalse(calendrier.estLibre(20,10), "20/10 devrait être occupé");
 		assertFalse(calendrier.reserver(20,10), "reservation du 20/10 ne devrait pas être possible");
 	}
 
