4035ecbf57af8739861bc3266f0437fc407737c2
lanternen <leo.audouin@univ-tlse3.fr>
2024-04-24 10:35:33 +0200

    TP Commun POO, les 3 tests de CalendrierAnnuelTest passent

--- src/model/CalendrierAnnuel.java
+++ src/model/CalendrierAnnuel.java
@@ -6,7 +6,7 @@ public class CalendrierAnnuel {
 	public CalendrierAnnuel() {
 		
 		this.calendrier[0] = new Mois("Janvier", 31);
-		this.calendrier[1] = new Mois("Fï¿½vrier", 28);
+		this.calendrier[1] = new Mois("Fevrier", 28);
 		this.calendrier[2] = new Mois("Mars", 31);
 		this.calendrier[3] = new Mois("Avril", 30);
 		this.calendrier[4] = new Mois("Mai", 31);
@@ -16,21 +16,22 @@ public class CalendrierAnnuel {
 		this.calendrier[8] = new Mois("Septembre", 30);
 		this.calendrier[9] = new Mois("Octobre", 31);
 		this.calendrier[10] = new Mois("Novembre", 30);
-		this.calendrier[11] = new Mois("Dï¿½cembre", 31);
+		this.calendrier[11] = new Mois("Decembre", 31);
 	}
 
 	
 	public boolean estLibre(int jour, int mois) {
-		return calendrier[mois-1].estLibre(jour);		// - 1 pour pas avoir out of bound
-	}
+		return !calendrier[mois-1].estLibre(jour-1);		// - 1 pour pas avoir out of bound
+	}													//return true si mois libre
 	
 	public boolean reserver(int jour, int mois) {
 		try {
-			calendrier[mois-1].reserver(jour);			// - 1 pour pas avoir out of bound
-			return true;
-		} catch (Throwable throwable) {
+			calendrier[mois-1].reserver(jour-1);			// - 1 pour pas avoir out of bound
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
 			return false;
 		}
+		return true;
 	}
 	
 	
@@ -45,20 +46,19 @@ public class CalendrierAnnuel {
 		
 		private Mois (String nom, int nbJours) {
 			this.nom = nom;
-			this.jours = new boolean[nbJours];	// ï¿½norme doute ici
-												//non c'est bon, mais pas besoin de faire une boucle pour mettre false
-												//maintenant, en java, c'est mis par dï¿½faut ï¿½ false
+			this.jours = new boolean[nbJours];	// pas besoin de faire une boucle pour mettre false
+												//maintenant, en java, c'est mis par defaut a false										
 		}
 		
 		private boolean estLibre(int jour) {
-			return !(jours[jour -1 ]);		//court, ï¿½lï¿½gant
+			return jours[jour];				//renvoie false si libre, true si deja occupé
 		}
 		
-		private void reserver(int jour) {
+		private void reserver(int jour) throws IllegalStateException {
 										// - 1 pour pas avoir out of bound
-			if (estLibre(jour))		//si true, ï¿½a veut dire qu'il y a dï¿½jï¿½ une rï¿½servation
+			if (estLibre(jour))		//si true, ca veut dire qu'il y a deja une reservation
 			{
-				throw new IllegalStateException ("dï¿½jï¿½ rï¿½servï¿½");
+				throw new IllegalStateException ("deja reserve");
 			} else {
 				jours[jour] = true;
 			}
